7e84a358345d61bb57f38cda27a562dcf6d61498
OKHYUN LEE <babylee2002@gmail.com>
2019-09-19 14:10:45 -0400

    commented out fizzbuzz

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -20,16 +20,28 @@ public class DemoApplication {
 		System.out.println(point2);
 		System.out.println("**********");
 
-		Scanner input = new Scanner(System.in);
 
-		double principle = input.nextDouble();
-		String currencyFormat = NumberFormat.getCurrencyInstance().format(principle);
-		double monthly = input.nextDouble()/100/12;
-		double duration = input.nextDouble() * 12;
-		System.out.println("Principle: "+currencyFormat);
-		System.out.println("Interest: "+monthly);
-		System.out.println("Duration: "+duration);
-		double mortgage = principle * (monthly * Math.pow((1+monthly),duration))/(Math.pow((1+monthly),duration)-1);
-		System.out.println("Your Mortgage is $" + Math.round(mortgage) +"/month");
+//		Scanner input = new Scanner(System.in);
+//
+//		byte userInput = input.nextByte();
+//		if(userInput % 3 == 0 && userInput % 5 == 0){
+//			System.out.println("fizzbuzz");
+//		} else if (userInput % 5 == 0){
+//			System.out.println("buzz");
+//		} else if (userInput % 3 == 0){
+//			System.out.println("fizz");
+//		} else {
+//			System.out.println(userInput);
+//		}
+//
+//		double principle = input.nextDouble();
+//		String currencyFormat = NumberFormat.getCurrencyInstance().format(principle);
+//		double monthly = input.nextDouble()/100/12;
+//		double duration = input.nextDouble() * 12;
+//		System.out.println("Principle: "+currencyFormat);
+//		System.out.println("Interest: "+monthly);
+//		System.out.println("Duration: "+duration);
+//		double mortgage = principle * (monthly * Math.pow((1+monthly),duration))/(Math.pow((1+monthly),duration)-1);
+//		System.out.println("Your Mortgage is $" + Math.round(mortgage) +"/month");
 	}
 }
